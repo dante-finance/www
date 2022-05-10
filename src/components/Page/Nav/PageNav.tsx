226bd4164e0fbb1702aafb9503828138762e4141
Marco Pasqualetti <marco.pasqualetti@live.com>
2022-05-11 00:47:33 +0200

    feat(PageHeader): use i18n

--- src/components/Page/Nav/PageNav.tsx
+++ src/components/Page/Nav/PageNav.tsx
@@ -1,17 +1,20 @@
 import Grid from '@mui/material/Grid';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import { PageNavConnect } from './PageNavConnect';
 
 export function PageNav(): JSX.Element {
+  const { t } = useTranslation(['generic']);
+
   return (
     <Grid justifyContent="space-between" container>
       <Grid item>
-        <Link to="/">Dante Finance</Link>
+        <Link to="/">{t('generic:site_title')}</Link>
       </Grid>
 
       <Grid item>
-        <Link to="/vaults">Vaults</Link>
+        <Link to="/vaults">{t('generic:page_header.navigation.vaults')}</Link>
         <PageNavConnect />
       </Grid>
     </Grid>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,12 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { Page } from 'components/Page/Page';
 
 export default function Home(): JSX.Element {
-  const { t } = useTranslation(['generic'] as const);
+  const { t } = useTranslation(['generic']);
 
   return (
     <Page>
-      <h1>Home</h1>
-      <p>{t('generic:hello')}</p>
+      <h1>{t('generic:home.title')}</h1>
     </Page>
   );
 }
