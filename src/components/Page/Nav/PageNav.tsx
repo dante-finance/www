07418c4f0899a1ba5e135a9a00fe90d325096555
Marco Pasqualetti <marco.pasqualetti@live.com>
2022-05-21 10:15:12 +0200

    fix(links): use Link component

--- src/components/Page/Nav/PageNav.tsx
+++ src/components/Page/Nav/PageNav.tsx
@@ -1,14 +1,17 @@
 import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
 
 import { PageNavConnect } from './PageNavConnect';
 
 export function PageNav(): JSX.Element {
   return (
     <Grid justifyContent="space-between" container>
-      <Grid item>Dante Finance</Grid>
+      <Grid item>
+        <Link to="/">Dante Finance</Link>
+      </Grid>
 
       <Grid item>
-        <a href="/vaults">Vaults</a>
+        <Link to="/vaults">Vaults</Link>
         <PageNavConnect />
       </Grid>
     </Grid>
--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 import styles from './NotFound.module.scss';
 
 export function NotFound(): JSX.Element {
   return (
     <article className={styles['not-found']}>
       <h1>URL Not Found.</h1>
-      <a href="/">Go back home.</a>
+      <Link to="/">Go back home.</Link>
     </article>
   );
 }
